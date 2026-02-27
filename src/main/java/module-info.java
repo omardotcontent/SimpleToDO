c4d8d1854513846033554b464461cfcf637a082f
Omar Mohamed <omartemhosma7@gmail.com>
2026-02-27 22:34:19 +0200

    remove task template from FXML and update module exports for controllers

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module omar.projects.simpletodo {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens omar.projects.simpletodo to javafx.fxml;
+    opens omar.projects.simpletodo.controllers to javafx.fxml;
     exports omar.projects.simpletodo;
 }
