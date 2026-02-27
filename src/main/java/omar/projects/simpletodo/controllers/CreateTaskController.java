98ab4dcbb55f6699ae760073fdd8381d4bb9cab5
Omar Mohamed <omartemhosma7@gmail.com>
2026-02-27 23:13:52 +0200

    add CreateTaskController for task creation and editing, update createTask.fxml for new controller and fields

---
+++ src/main/java/omar/projects/simpletodo/controllers/CreateTaskController.java
@@ -0,0 +1,80 @@
+package omar.projects.simpletodo.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import omar.projects.simpletodo.handlers.taskHandler;
+import omar.projects.simpletodo.objects.Task;
+
+public final class CreateTaskController {
+
+    private Task currentTask;
+
+    @FXML
+    private Button applyButton;
+
+    @FXML
+    private Button createButton;
+
+    @FXML
+    private Button deleteButton;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private TextField descriptionField;
+
+    @FXML
+    public void initialize() {
+        nameField.sceneProperty().addListener((obs, oldScene, newScene) -> {
+            if (newScene != null) {
+                newScene.windowProperty().addListener((obs2, oldWindow, newWindow) -> {
+                    if (newWindow != null) {
+                        newWindow.setOnHiding(e -> {
+                            if (currentTask != null) {
+                                taskHandler.removeTask(currentTask);
+                                currentTask = null;
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    }
+
+    public void init(final Task task) {
+        if (task != null) {
+            currentTask = task;
+            applyButton.setVisible(true);
+            deleteButton.setVisible(true);
+            createButton.setVisible(false);
+            createButton.setManaged(false);
+            nameField.setText(task.getName());
+            descriptionField.setText(task.getDescription());
+            applyButton.setOnAction(e -> {
+                task.setName(nameField.getText());
+                task.setDescription(descriptionField.getText());
+            });
+            deleteButton.setOnAction(e -> {
+                taskHandler.removeTask(task);
+                currentTask = null;
+                ((Stage) nameField.getScene().getWindow()).close();
+            });
+        } else {
+            applyButton.setVisible(false);
+            deleteButton.setVisible(false);
+            applyButton.setManaged(true);
+            deleteButton.setManaged(false);
+            createButton.setVisible(true);
+            createButton.setOnAction(e -> {
+                        new Task(nameField.getText(), descriptionField.getText(), false, taskHandler.getTasks());
+                        ((Stage) nameField.getScene().getWindow()).close();
+                    }
+            );
+
+        }
+
+    }
+}
