a481bc45abd5f3faa1eacdd2eea361b85574a375
Omar Mohamed <omartemhosma7@gmail.com>
2026-02-27 22:34:26 +0200

    add Task class for dynamic task creation and update mainPage to use Task objects

--- src/main/java/omar/projects/simpletodo/controllers/mainPage.java
+++ src/main/java/omar/projects/simpletodo/controllers/mainPage.java
@@ -3,18 +3,17 @@ package omar.projects.simpletodo.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import omar.projects.simpletodo.objects.Task;
 
 public class mainPage {
-
-    @FXML
-    private BorderPane taskTemplate;
 
     @FXML
     private VBox tasks;
 
     @FXML
     public void initialize() {
-
+        new Task("Task 1", "This is the first task", false, tasks);
+        new Task("Task 2", "This is the second task", true, tasks);
     }
 
 }
---
+++ src/main/java/omar/projects/simpletodo/objects/Task.java
@@ -0,0 +1,121 @@
+package omar.projects.simpletodo.objects;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+
+public class Task {
+
+    private String name;
+    private String description;
+    private boolean checked;
+
+    private BorderPane taskBox;
+    private Text nameText;
+    private Text descriptionText;
+    private CheckBox checkBox;
+
+
+    public Task(final String name, final String description, final boolean checked, final VBox tasks) {
+        this.name = name;
+        this.description = description;
+        this.checked = checked;
+        setupBorderPane(tasks);
+    }
+
+    private void setupBorderPane(final VBox tasks) {
+        taskBox = new BorderPane();
+
+        taskBox.getStyleClass().add("todo-item");
+
+        nameText = new Text();
+        nameText.setText(name);
+        nameText.setTextAlignment(TextAlignment.LEFT);
+        nameText.getStyleClass().add("title");
+
+        descriptionText = new Text();
+        descriptionText.setText(description);
+        descriptionText.setTextAlignment(TextAlignment.LEFT);
+        descriptionText.getStyleClass().add("description");
+
+        checkBox = new CheckBox();
+        checkBox.setSelected(checked);
+
+        checkBox.setMinHeight(Region.USE_COMPUTED_SIZE);
+        checkBox.setMinWidth(Region.USE_COMPUTED_SIZE);
+
+        checkBox.setMaxHeight(Double.MAX_VALUE);
+        checkBox.setMaxWidth(Double.MAX_VALUE);
+
+        checkBox.setPrefHeight(Region.USE_COMPUTED_SIZE);
+        checkBox.setPrefWidth(Region.USE_COMPUTED_SIZE);
+
+        taskBox.setRight(checkBox);
+
+
+        final VBox info = new VBox();
+        info.getChildren().add(nameText);
+        info.getChildren().add(descriptionText);
+
+        info.setMinHeight(Region.USE_COMPUTED_SIZE);
+        info.setMinWidth(Region.USE_COMPUTED_SIZE);
+
+        info.setMaxHeight(Double.MAX_VALUE);
+        info.setMaxWidth(Double.MAX_VALUE);
+
+        info.setPrefHeight(Region.USE_COMPUTED_SIZE);
+        info.setPrefWidth(Region.USE_COMPUTED_SIZE);
+
+        info.setAlignment(Pos.CENTER_LEFT);
+
+
+        taskBox.setLeft(info);
+
+        taskBox.setMinHeight(Region.USE_COMPUTED_SIZE);
+        taskBox.setMinWidth(Region.USE_COMPUTED_SIZE);
+        taskBox.setMaxHeight(Double.MAX_VALUE);
+        taskBox.setMaxWidth(Double.MAX_VALUE);
+
+        taskBox.setPrefHeight(Region.USE_COMPUTED_SIZE);
+        taskBox.setPrefWidth(Region.USE_COMPUTED_SIZE);
+
+        tasks.getChildren().add(taskBox);
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public BorderPane getTaskBox() {
+        return taskBox;
+    }
+
+    public boolean isChecked() {
+        return checked;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+        nameText.setText(name);
+    }
+
+    public void setDescription(final String description) {
+        this.description = description;
+        descriptionText.setText(description);
+    }
+
+    public void setChecked(final boolean checked) {
+        this.checked = checked;
+        checkBox.setSelected(checked);
+    }
+}
