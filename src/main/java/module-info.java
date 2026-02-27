66d291bb4413ed9c3939dc3bda74e410e6c54b11
Omar Mohamed <omartemhosma7@gmail.com>
2026-02-27 23:14:11 +0200

    add java.desktop module requirement to module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module omar.projects.simpletodo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
     opens omar.projects.simpletodo to javafx.fxml;
     opens omar.projects.simpletodo.controllers to javafx.fxml;
